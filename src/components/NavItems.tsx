9b91b0bb7996d6cdea7f43d6850e2c551214271b
amh6281 <amh6281@naver.com>
2023-12-04 19:29:18 +0900

    onClickOutside 기능 구현 2023.12.04

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,15 +1,33 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/useOnClickOutside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+
+    // 키보드 이벤트 감지
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
   const isAnyOpen = activeIndex !== null;
 
   const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
 
   return (
     <div className="flex gap-4 h-full" ref={navRef}>
