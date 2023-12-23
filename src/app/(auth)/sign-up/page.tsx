3c2ac474fcda3c3458b71798e6e0b62f56277755
amh6281 <amh6281@naver.com>
2023-12-23 10:06:20 +0900

    react-hook-form을 통한 폼 유효성 검사 및 submit 구현

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,14 +1,30 @@
 "use client";
 
 import { Icons } from "@/components/Icons";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 import { Label } from "../../../components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  AuthCredentialsValidator,
+  TAuthCredentialsValidator,
+} from "@/lib/validators/account-credentials-validator";
 
 const Page = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TAuthCredentialsValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
+  });
+
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {};
+
   return (
     <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
@@ -26,12 +42,13 @@ const Page = () => {
           </Link>
         </div>
         <div className="grid gap-6">
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="grid gap-2">
               <div className="grid gap-1 py-2">
                 <Label htmlFor="email">Email</Label>
                 <Input
-                  className={cn({ "focus-visible:ring-red-500": true })}
+                  {...register("email")}
+                  className={cn({ "focus-visible:ring-red-500": errors.email })}
                   placeholder="ex@example.com"
                 />
               </div>
@@ -39,10 +56,14 @@ const Page = () => {
               <div className="grid gap-1 py-2">
                 <Label htmlFor="password">Password</Label>
                 <Input
-                  className={cn({ "focus-visible:ring-red-500": true })}
+                  {...register("password")}
+                  className={cn({
+                    "focus-visible:ring-red-500": errors.password,
+                  })}
                   placeholder="Password"
                 />
               </div>
+              <Button>Sign up</Button>
             </div>
           </form>
         </div>
