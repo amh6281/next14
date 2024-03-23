a14d20743feacbc1499123a826b1eed693e0a08c
amh6281 <amh6281@naver.com>
2024-03-23 15:58:22 +0900

    project reset

--- src/trpc/trpc.ts
+++
@@ -1,8 +0,0 @@
-import { initTRPC } from "@trpc/server";
-
-// initTRPC을 사용하여 tRPC 서버를 초기화
-const t = initTRPC.context().create();
-
-// tRPC 서버의 라우터와 프로시저를 내보내기
-export const router = t.router;
-export const publicProcedure = t.procedure;
