dd42a6fad3cfa9742041515d72adea049cb6d33e
amh6281 <amh6281@naver.com>
2024-03-23 16:41:21 +0900

    Feat: AfterLoginLayout 추가

---
+++ src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,9 @@
+const AfterLoginLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return <div>afterLoginLayout{children}</div>;
+};
+
+export default AfterLoginLayout;
